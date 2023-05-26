f9dbb302cf9b894a87598b06e48f5badadf40022
Senuji Pathirage <cb010982@students.apiit.lk>
2023-05-26 21:53:42 +0530

    Dropdown component commit

--- frontend/src/Dropdown.js
+++ frontend/src/Dropdown.js
@@ -1,37 +1,73 @@
-import React from 'react';
-import './Dropdown.css'; 
-import { useState } from "react";
+import './Dropdown.css';
+import React, { useState } from "react";
 
+const DropDown = {
+  DISTRICTDROPDOWN: "districtdropdown",
+  BLOODTYPEDROPDOWN: "bloodtypedropdown",
+};
 
 function Dropdown(props) {
-  const [openDropdown, setOpenDropdown] = useState(false);
-  const [optionSelected, setOptionSelected] = useState("");
 
-  const handleClick = (choice) => {
-    setOptionSelected(choice);
-    setOpenDropdown(false);
-    props.onSelect(choice);
+
+    const [selectedOption, setSelectedOption] = useState('');
+
+    const handleSelectChange = (event) => {
+      setSelectedOption(event.target.value);
+    };
+    if (props.dropdown === DropDown.DISTRICTDROPDOWN) {
+      return (
+        <div>
+          <select value={selectedOption} onChange={handleSelectChange} className="districtdrop" >
+          <option value="1">Ampara</option>
+          <option value="2">Anuradhapura</option>
+          <option value="3">Badulla</option>
+          <option value="4">Batticaloa</option>
+          <option value="5">Colombo</option>
+          <option value="6">Galle</option>
+          <option value="7">Gampaha</option>
+          <option value="8">Hambantota</option>
+          <option value="9">Jaffna</option>
+          <option value="10">Kalutara</option>
+          <option value="11">Kandy</option>
+          <option value="12">Kegalla</option>
+          <option value="13">Kilinochchi</option>
+          <option value="14">Kurunegala</option>
+          <option value="15">Mannar</option>
+          <option value="16">Matale</option>
+          <option value="17">Matara</option>
+          <option value="18">Moneragala</option>
+          <option value="19">Mullaitivu</option>
+          <option value="20">Nuwara Eliya</option>
+          <option value="21">Polonnaruwa</option>
+          <option value="22">Puttalam</option>
+          <option value="23">Ratnapura</option>
+          <option value="24">Trincomalee</option>
+          <option value="25">Vavuniya</option>
+        </select>
+        </div>
+      );
+    }
+
+    if (props.dropdown === DropDown.BLOODTYPEDROPDOWN) {
+
+      return (
+        <div>
+        <select value={selectedOption} onChange={handleSelectChange} className="bloodtypedrop" >
+        <option value="A+">A+</option>
+        <option value="A-">A-</option>
+        <option value="B+">B+</option>
+        <option value="B-">B-</option>
+        <option value="AB+">AB+</option>
+        <option value="AB-">AB-</option>
+        <option value="O+">O+</option>
+        <option value="O-">O-</option>
+      </select>
+        </div>
+      );
+    
   };
 
-  return (
-    <div className="dropdown">
-      <button className="dropdownbutton" onClick={() => setOpenDropdown(!openDropdown)}>
-        {props.title}
-      </button>
-      {openDropdown && (
-        <div className="dropdowninfo">
-          {props.items.map((item, index) => (
-            <div key={index} onClick={() => handleClick(item)}>
-              {item}
-            </div>
-          ))}
-        </div>
-      )}
-      {optionSelected && (
-        <div className="option">You selected: {optionSelected}</div>
-      )}
-    </div>
-  );
-}
+
+  }
 
 export default Dropdown;
