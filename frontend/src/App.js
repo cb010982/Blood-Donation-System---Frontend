9d74bd987487e2ec4107e4b7d2ae31541202bd81
Dulanmi Himansa De Silva <cb011009@students.apiit.lk>
2023-06-15 13:33:02 +0530

    Adding the finalized set of paths in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,122 +1,129 @@
 
-import Location2 from './Location2';
-import BloodCountChart from './BloodCountChart';
 import {BrowserRouter as Router, Route} from "react-router-dom"
-import Location1 from './Location1';
-import Donorpoints from './Donorpoints';
-import Dashboards from './Dashboards';
 import Navigation from './Navigation';
-import Table from './Table';
-import SignUp from './SignUp';
-import Acceptedrequests from './Acceptedrequests';
-import Pendingrequests from './Pendingrequests';
-import FAQs from './FAQs';
-import Search from './Search';
 import LoginAndSignUp from './LoginAndSignUp';
+import About from "./About";
+import FAQs from "./FAQs";
+import Table from "./Table";
+import Dashboards from "./Dashboards";
+import Donorpoints from './Donorpoints';
+import BloodCountChart from './BloodCountChart';
+import { UserTypes } from "./utils/Enums";
+import { TableNames } from "./utils/Enums";
+import { FormNames } from "./utils/Enums";
 
 
 function App() {
   return (
     <Router>
       <Route exact path='/'>
-        <Navigation user="home"/>
-        <LoginAndSignUp page="donorSignup"/>
+      <Navigation user={UserTypes.BLOODBANK} />
       </Route>
-    <Route path='/donorLoginPage'>
-       <Navigation user="home"/>
-       <LoginAndSignUp page="donorLogin"/>
-    </Route>
-    <Route path='/adminLoginPage'>
-       <Navigation user="home"/>
-      <LoginAndSignUp page="adminLogin"/>
-    </Route>
-    <Route path='/hospitalLoginPage'>
-       <Navigation user="home"/>
-       <LoginAndSignUp page="hospitalLogin"/>
-    </Route>
-   <Route path='/bloodBankLoginPage'>
-      <Navigation user="home"/>
-      <LoginAndSignUp page="bloodBankLogin"/>
-   </Route>
-   <Route path='/donorSignUpPage'>
-      <Navigation user="home"/>
-      <LoginAndSignUp page="donorSignup"/>
-   </Route>
-   <Route path='/adminSignUpPage'>
-     <Navigation user="home"/>
-     <LoginAndSignUp page="adminSignup"/>
-   </Route>
-  <Route path='/hospitalSignUpPage'>
-     <Navigation user="home"/>
-     <LoginAndSignUp page="hospitalSignup"/>
-  </Route>
-  <Route path='/bloodBankSignUpPage'>
-      <Navigation user="home"/>
-      <LoginAndSignUp page="bloodBankSignup"/>
-  </Route>
-      {/*<Route exact path='/'>
-      <Dashboards/>
-  </Route>*/}
-      <Route path='/Location1'>
-        <Location1/>
+      <Route exact path='/about'>
+      <About user={UserTypes.ABOUT} />
       </Route>
-      <Route path='/Location2'>
-        <Navigation user="donor"/>
-        <Location2/>
-        <Table tableName="DONOR LOCATION"/>
+      <Route path='/aboutDonor'>
+        <About user={UserTypes.DONOR}/>
       </Route>
-      <Route path='/Donorpoints'>
-        <Donorpoints/>
+      <Route path='/aboutHospital'>
+        <About user={UserTypes.HOSPITAL}/>
       </Route>
-      <Route path='/Dashboards'>
-        <Dashboards/>
+      <Route path='/aboutBloodBank'>
+        <About user={UserTypes.BLOODBANK}/>
       </Route>
-      <Route path='/BloodBankChart'>
-        <Navigation user="bloodbank"/>
-        <BloodCountChart/>
-      </Route>
-      <Route path='/HospitalChart'>
-      <Navigation user="hospital"/>
-        <BloodCountChart/>
-      </Route>
-      <Route path='/Navigation'>
-        <Navigation/>
-      </Route>
-      <Route path='/Table'>
-        <Table/>
-      </Route>
-      <Route path='/SignUp'>
-        <SignUp/>
-      </Route>
-      <Route path='/Acceptedrequests'>
-        <Acceptedrequests/>
-      </Route>
-      <Route path='/Pendingrequests'>
-        <Pendingrequests/>
+      <Route path='/aboutAdmin'>
+        <About user={UserTypes.ADMIN}/>
       </Route>
       <Route path='/FAQs'>
-        <FAQs/>
+         <FAQs/>
       </Route>
-      <Route path='/Search'>
-        <Search/>
+      <Route path='/donorlocation'>
+        <Navigation user={UserTypes.DONOR}/>
+        <Table tableName={TableNames.DONORLOCATION}/>
       </Route>
-      <Route path='/BloodBankSearch'>
-        <Navigation user="hospital"/>
-        <Table tableName="BLOOD BANK SEARCH"/>
+      <Route path='/donorPoints'>
+        <Donorpoints/>
       </Route>
-      <Route path='/Donorsearch'>
-      <Navigation user="bloodbank"/>
-        <Table tableName="DONOR SEARCH"/>
+      <Route path='/bloodBankChart'>
+        <Navigation user={UserTypes.BLOODBANK}/>
+        <BloodCountChart/>
       </Route>
-      <Route path='/Donorhistory'>
-      <Navigation user="donor"/>
-        <Table tableName="DONOR HISTORY"/>
+      <Route path='/hospitalChart'>
+        <Navigation user={UserTypes.HOSPITAL}/>
+        <BloodCountChart/>
       </Route>
-      <div className='foot'>
-      <footer></footer>
-      </div>
+
+      <Route path='/donorLoginPage'>
+         <LoginAndSignUp  page={FormNames.DONOR_LOGIN}/>
+      </Route>
+      <Route path='/donorSignUpPage'>
+         <LoginAndSignUp page={FormNames.DONOR_SIGNUP}/>
+      </Route>
+      <Route path='/adminLoginPage'>
+          <LoginAndSignUp  page={FormNames.ADMIN_LOGIN}/>
+      </Route>
+      <Route path='/adminSignUpPage'>
+          <LoginAndSignUp page={FormNames.ADMIN_SIGNUP}/>
+      </Route>
+      <Route path='/hospitalLoginPage'>
+          <LoginAndSignUp  page={FormNames.HOSPITAL_LOGIN}/>
+      </Route>
+      <Route path='/hospitalSignUpPage'>
+         <LoginAndSignUp page={FormNames.HOSPITAL_SIGNUP}/>
+      </Route>
+      <Route path='/bloodBankLoginPage'>
+         <LoginAndSignUp  page={FormNames.BLOODBANK_LOGIN}/>
+      </Route>
+      <Route path='/bloodBankSignUpPage'>
+         <LoginAndSignUp page={FormNames.BLOODBANK_SIGNUP}/>
+      </Route>
+
+      <Route path='/donorHistory'>
+         <Navigation user={UserTypes.DONOR} />
+         <Table tableName={TableNames.DONORHISTORY} />
+      </Route>
+      <Route path='/donorSearch'>
+         <Navigation user={UserTypes.BLOODBANK}/>
+         <Table tableName={TableNames.DONORSEARCH}/>
+      </Route>
+      <Route path='/bloodBankSearch'>
+         <Navigation user={UserTypes.HOSPITAL}/>
+         <Table tableName={TableNames.BLOODBANKSEARCH}/>
+      </Route>
+      <Route path='/hospitalPending'>
+         <Navigation user={UserTypes.ADMIN}/>
+         <Table tableName={TableNames.HOSPITALPENDING}/>
+      </Route>
+      <Route path='/hospitalAccepted'>
+         <Navigation user={UserTypes.ADMIN}/>
+         <Table tableName={TableNames.HOSPITALACCEPTED}/>
+      </Route>
+      <Route path='/bloodBankPending'>
+        <Navigation user={UserTypes.ADMIN}/>
+        <Table tableName={TableNames.BLOODBANKPENDING}/>
+      </Route>
+      <Route path='/bloodBankAccepted'>
+        <Navigation user={UserTypes.ADMIN}/>  
+        <Table tableName={TableNames.BLOODBANKACCEPTED}/>
+      </Route>
+
+
+      <Route path='/donorDashboard'>
+        <Dashboards user={UserTypes.DONOR}/>
+      </Route>
+      <Route path='/adminDashboard'>
+        <Dashboards user={UserTypes.ADMIN}/>
+      </Route>
+      <Route path='/hospitalDashboard'>
+        <Dashboards user={UserTypes.HOSPITAL}/>
+      </Route>
+      <Route path='/bloodBankDashboard'>
+        <Dashboards user={UserTypes.BLOODBANK}/>
+      </Route>
     </Router>
-  );
+
+    
+  )
 }
 
 export default App;
