2c092a473a05048db8337536c2707e0d01947aa6
Senuji Pathirage <cb010982@students.apiit.lk>
2023-06-15 13:41:57 +0530

    Enum commit

---
+++ frontend/src/utils/Enums.js
@@ -0,0 +1,67 @@
+export const UserTypes = {
+    DONOR: 'donor',
+    ADMIN: 'admin',
+    HOSPITAL: 'hospital',
+    BLOODBANK: 'bloodBank',
+    ABOUT:'About'
+  };
+
+export const TableNames ={
+  DONORHISTORY: "DONOR HISTORY",
+  DONORSEARCH: "DONOR SEARCH",
+  BLOODBANKSEARCH: "BLOOD BANK SEARCH",
+  DONORLOCATION:"NEAREST BLOOD BANK",
+  BLOODBANKPENDING:"BLOOD BANK PENDING REQUESTS",
+  BLOODBANKACCEPTED:"BLOOD BANK ACCEPTED REQUESTS",
+  HOSPITALPENDING:"HOSPITAL PENDING REQUESTS",
+  HOSPITALACCEPTED:"HOSPITAL ACCEPTED REQUESTS"
+};
+
+export const FormNames = {
+  DONOR_LOGIN: "DONOR LOGIN",
+  DONOR_SIGNUP: "DONOR SIGNUP",
+  ADMIN_LOGIN: "ADMIN LOGIN",
+  ADMIN_SIGNUP: "ADMIN SIGNUP",
+  HOSPITAL_LOGIN: "HOSPITAL LOGIN",
+  HOSPITAL_SIGNUP: "HOSPITAL SIGNUP",
+  BLOODBANK_LOGIN: "BLOOD BANK LOGIN",
+  BLOODBANK_SIGNUP: "BLOOD BANK SIGNUP",
+
+};
+
+export const DropDown = {
+  DISTRICTDROPDOWN: "districtdropdown",
+  BLOODTYPEDROPDOWN: "bloodtypedropdown",
+};
+
+export const InputType= {
+  TEXT: "text",
+  RADIO: "radio",
+  DATE: "date",
+  DROPDOWN: "dropdown",
+  TEXTAREA: "textarea",
+  PASSWORD: "password",
+  NUMBER:"number",
+};
+
+export const InputFieldName={
+  NIC: "NIC",
+  FULLNAME: "Full Name",
+  USERNAME:"Username",
+  GENDER: "Gender",
+  DONORBLOODTYPE:"Donor Blood Type",
+  BLOODTYPE: "Blood Type",
+  DISTRICT:"District Located In",
+  HOSPITALNAME: "Name Of Hospital",
+  BLOODBANKNAME:"Name Of Blood Bank",
+  BIRTHDATE:"Date Of Birth",
+  DATE:"Date",
+  DONORADDRESS:"Donor Address",
+  ADDRESS:"Address",
+  TELEPHONE:"Telephone Number",
+  PASSWORD: "Password",
+  LOGINPASSWORD: "Login Password",
+  PINTS:"Pints",
+  REWARD:"Reward Points",
+  CRITICALLEVEL:"Critical level"
+ }
